0c5a46f6df4cc4ec1bb4efb28d45ea6ddeb94f52
XunStarRock <1308435433@qq.com>
2023-10-15 23:03:23 +0800

    添加原理解析

--- lib/Sty.gd
+++ lib/Sty.gd
@@ -11,19 +11,25 @@ class_name Sty
 
 # 样式解析
 static func parse_style(ctl:Control,style_str:String):
+	# 创建ConfigFile实例
 	var cfg = ConfigFile.new()
+	# 在解析前替换冒号和封号，使格式转为标准ConfigFile形式，从而保证被正确解析
 	var err = cfg.parse(style_str.replace(":","=\"").replace(";","\""))
 	if err == OK: # 解析成功
+		# 遍历节和旗下的键
 		for section in cfg.get_sections():
 			for key in cfg.get_section_keys(section):
-				var val = cfg.get_value(section,key)
+				var val = cfg.get_value(section,key) # 获取键的值
+				# 根据键的名称分别处理
 				match key:
 					"font_size":
 						pass
 						
-					"color":
+					# 根据节的名称更近一步进行细节处理
 						match section:
+							# 节名对应的是一种控件的交互或状态
 							"normal":
+								# 添加属性覆盖
 								ctl.add_theme_color_override("font_color",Color(val))
 							"hover","pressed","disabled","focus":
 								ctl.add_theme_color_override("font_%s_color" % section,Color(val))
@@ -99,9 +105,12 @@ static func parse_style(ctl:Control,style_str:String):
 # 获取控件对应名称的样式盒
 static func get_stylebox(ctl:Control,name:String) -> StyleBoxFlat:
 	var stylebox:StyleBoxFlat
+	# 如果已经存在覆盖的样式盒
 	if ctl.has_theme_stylebox_override(name):
 		stylebox = ctl.get_theme_stylebox(name)
 	else:
+		# 创建一个新的样式盒 - 默认为StyleBoxFlat类型
 		stylebox= StyleBoxFlat.new()
+		# 添加为覆盖样式盒
 		ctl.add_theme_stylebox_override(name, stylebox)
-	return stylebox
+	return stylebox # 返回对样式盒的引用，以用于后续操作
